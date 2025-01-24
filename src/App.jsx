5af0c5e548df28a5e7ba1951e50d557cfa401765
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2025-01-24 04:47:01 +0100

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="grid max-w-[1400px] flex-col gap-10   px-6 py-8 md:h-[100vh] md:grid-cols-[1fr_35%]  ">
+      Hello here
+    </div>
+  );
+}
