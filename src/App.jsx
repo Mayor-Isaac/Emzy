48a704cd5009709f8b9a9b56acef11383fe2b334
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2025-01-27 22:35:40 +0100

    Navbar started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
+import NavBar from './components/NavBar';
 
 export default function App() {
-  return (
-    <div className="grid max-w-[1400px] flex-col gap-10   px-6 py-8 md:h-[100vh] md:grid-cols-[1fr_35%]  ">
-      Hello here
-    </div>
-  );
+  return <NavBar />;
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <div className="align-center flex justify-between border border-yellow-600 px-3 py-4">
+      <div className="logo w-[4rem]  text-center text-2xl font-extrabold ">
+        Emzy
+      </div>
+      <button>Menu</button>
+    </div>
+  );
+}
+
+export default NavBar;
