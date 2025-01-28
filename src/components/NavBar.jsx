3238e89dfb6df514283431cdcb0ea1506b8b1799
Mayor-Isaac <ogunyilekaferanmi@gmail.com>
2025-01-28 16:34:29 +0100

    28-01

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react';
+
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const media = window.innerWidth >= 768;
+  console.log(media);
+
+  useEffect(() => {}, [media]);
+
+  function handleMenu() {
+    setIsMenuOpen((show) => !show);
+  }
   return (
-    <div className="align-center flex justify-between border border-yellow-600 px-3 py-4">
+    <div className="flex items-center justify-between border border-yellow-600 px-6 py-4">
       <div className="logo w-[4rem]  text-center text-2xl font-extrabold ">
-        Emzy
+        Logo
       </div>
-      <button>Menu</button>
+      {media ||
+        (isMenuOpen && (
+          <div className="flex items-center space-x-4">
+            <div className="px-3">About</div>
+            <div className="px-3">Projects</div>
+            <div className="px-3">Contact</div>
+          </div>
+        ))}
+      <button className="md:hidden" onClick={handleMenu}>
+        Menu
+      </button>
     </div>
   );
 }
